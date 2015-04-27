fec4350da55332a8ef5bac6b8124059e1355cc8a
Robert Hodgen <roberthodgen@gmail.com>
2015-04-27 14:20:39 -0400

    Grunt and NPM package dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,49 +9,31 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		clean: {
-			build: 'static/vendor/*'
+			build: 'src/static/vendor/*'
 		}, copy: {
-			ngwp: {
-				expand: true,
-				cwd: 'src',
-				src: '**/*',
-				dest: 'dist',
-				filter: 'isFile'
-			}, bootstrap: {
+			bootstrap: {
 				expand: true,
 				cwd: 'node_modules/bootstrap/dist',
 				src: '**/*',
-				dest: 'static/vendor/bootstrap',
+				dest: 'src/static/vendor/bootstrap',
 				filter: 'isFile'
 			}, jquery: {
 				expand: true,
 				cwd: 'node_modules/jquery/dist',
 				src: '**/*',
-				dest: 'static/vendor/jquery',
+				dest: 'src/static/vendor/jquery',
 				filter: 'isFile'
 			}, angular: {
 				expand: true,
 				cwd: 'node_modules/angular',
 				src: '**/*',
-				dest: 'static/vendor/angular',
+				dest: 'src/static/vendor/angular',
 				filter: 'isFile'
 			}, angular_route: {
 				expand: true,
 				cwd: 'node_modules/angular-route',
 				src: '**/*',
-				dest: 'static/vendor/angular-route',
-				filter: 'isFile'
-			}, angular_ui_router: {
-				expand: true,
-				cwd: 'node_modules/angular-ui-router/release',
-				src: '**/*',
-				dest: 'static/vendor/angular-ui-router',
-				filter: 'isFile'
-			}, angular_sanitize: {
-				expand: true,
-				cwd: 'node_modules/angular-sanitize',
-				src: '**/*',
-				dest: 'static/vendor/angular-sanitize',
+				dest: 'src/static/vendor/angular-route',
 				filter: 'isFile'
 			}
 		}
