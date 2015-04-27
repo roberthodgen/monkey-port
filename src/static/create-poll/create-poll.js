88be35b41ee7c393005610fc23c1162f3ec07ccd
Robert Hodgen <roberthodgen@gmail.com>
2015-04-27 15:42:27 -0400

    More work on the create poll form/directive

--- src/static/create-poll/create-poll.js
+++ src/static/create-poll/create-poll.js
@@ -5,7 +5,39 @@
 	app.directive('createPoll', function() {
 		return {
 			restrict: 'AE',
-			templateUrl: '/create-poll/detail.html'
+			templateUrl: '/create-poll/detail.html',
+			controller: function($scope) {
+				$scope.init = function() {
+					$scope.newPoll = {
+						responses: [
+							{
+								name: ''
+							}, {
+								name: ''
+							}
+						],
+						question: ''
+					}
+				};
+
+				$scope.addResponse = function() {
+					$scope.newPoll.responses.push({
+						name: ''
+					});
+				};
+
+				$scope.removeResponse = function(index) {
+					$scope.newPoll.responses.splice(index, 1);
+				};
+
+				$scope.createPoll = function() {
+					console.log('[mp.createPoll] $scope.createPoll: Called.');
+				};
+
+
+				// Init
+				$scope.init();
+			}
 		};
 	});
 
