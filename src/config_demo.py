f7e3dfb434b16d67fe427730af07856b1ffc780d
Robert Hodgen <roberthodgen@gmail.com>
2015-04-29 16:42:16 -0400

    Added default 1234 code to Robs Google Voice number

--- src/config_demo.py
+++ src/config_demo.py
@@ -12,3 +12,9 @@ PHONE_NUMBER = '+18504004949'
 ACCOUNT_SID = '### YOUR ACCOUNT SID HERE ###'
 
 AUTH_TOKEN = '### YOUR AUTH TOEKN HERE ###'
+
+""" `IN_CALL_CODES` stores telephone numbers (in E.164 format) in a key-value coded dictionary
+whereby the key is the numerical code and the value is the telephone number. """
+IN_CALL_CODES = {
+	'1234': '+16506877776'
+}
