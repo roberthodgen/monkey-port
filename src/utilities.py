90d448164c75817b510465d642b40bae0527b4d2
Robert Hodgen <roberthodgen@gmail.com>
2015-04-29 16:41:54 -0400

    Will return a verbose call status, lookup a telephone number for a given code

--- src/utilities.py
+++ src/utilities.py
@@ -10,6 +10,9 @@ import string
 
 import hashlib
 
+import config
+
+
 def generate_id():
     """ Returns a token of a given length. """
     return ''.join(random.SystemRandom().sample(
@@ -18,3 +21,17 @@ def generate_id():
 def secure_hash(a_str):
     """ Return a secure hash of `a_str`. """
     return hashlib.sha256(a_str).hexdigest()
+
+def verbose_call_status(call_status):
+	""" Return a verbose string (to speak) for a given call status as `call_status`. """
+	return {
+		'completed': 'The call has ended',
+		'busy': 'A busy signal was received when trying to connect',
+		'no-answer': 'The called party did not pick up before the timeout period passed',
+		'failed': 'The call has failed',
+		'canceled': 'The call was canceled before it was answered'
+	}.get(call_status, 'Call status unknown')
+
+def number_for_code(code):
+	""" Return a phone number or False for a given code as `code`. """
+	return config.IN_CALL_CODES.get(code, False)
