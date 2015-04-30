6f62bce87d987de145dc503187784561cacc90f0
Robert Hodgen <roberthodgen@gmail.com>
2015-04-29 23:12:01 -0400

    Added directives for polls

--- src/static/create-poll/create-poll.js
+++ src/static/create-poll/create-poll.js
@@ -2,11 +2,11 @@
 
 	var app = angular.module('mp.createPoll', []);
 
-	app.directive('createPoll', function() {
+	app.directive('createPoll', function(Poll) {
 		return {
 			restrict: 'AE',
 			templateUrl: '/create-poll/detail.html',
-			controller: function($scope) {
+			controller: function($scope, $location) {
 				$scope.init = function() {
 					$scope.newPoll = {
 						responses: [
@@ -32,6 +32,30 @@
 
 				$scope.createPoll = function() {
 					console.log('[mp.createPoll] $scope.createPoll: Called.');
+
+					var responses = [];
+
+					for (var i = 0; i < $scope.newPoll.responses.length; i++) {
+						responses.push($scope.newPoll.responses[i].name);
+					}
+
+					Poll.create(null, {
+						question: $scope.newPoll.question,
+						responses: responses
+					}, function(response) {
+						// HTTP 200-299 Status
+						if (angular.isObject(response)) {
+							if (response.hasOwnProperty('id')) {
+								$location.path('/poll/'+response.id).search({
+									preview: true
+								});
+							}
+						}
+						console.log(response);
+					}, function(response) {
+						// Error
+						console.log(response);
+					});
 				};
 
 
@@ -40,5 +64,27 @@
 			}
 		};
 	});
+
+	app.directive('pollDetail', function(Poll) {
+		return {
+			restrict: 'AE',
+			templateUrl: '/poll-detail/detail.html'
+		};
+	});
+
+	app.directive('pollPreview', function(Poll) {
+		return {
+			restrict: 'AE',
+			templateUrl: '/poll-detail/preview.html'
+		};
+	});
+
+	app.factory('Poll', function($resource) {
+		return $resource('/api/poll/:pollId', null, {
+			create: {
+				method: 'POST'
+			}
+		});
+	})
 
 })();
