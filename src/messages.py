774bf399e83c8341fb2f0057f5c826a688415179
Robert Hodgen <roberthodgen@gmail.com>
2015-05-01 12:10:13 -0400

    carry over to new line to keep Python 79-character line limit

--- src/messages.py
+++ src/messages.py
@@ -28,41 +28,40 @@ CLIENT = TwilioRestClient(config.ACCOUNT_SID, config.AUTH_TOKEN)
 @APP.route('/messages/incoming', methods=['GET', 'POST'])
 def incoming_message():
     """ Respond to an incoming message. """
-
     response = twilio.twiml.Response()
-
     from_number = request.values.get('From', False)
     if not from_number:
         from_number = 'Unknown'
-
     sms_message = request.values.get('Body', False)
     if not sms_message:
         print 'No SMS message received!'
         sms_message = 'No message content.'
-
     # Attempt to find a response for this body...
     pattern = re.compile('[bcdfghjkmnpqrstvwxyz0-9]{4}', re.I)
     match = re.match(pattern, sms_message)
     if match:
-
         response_key_id = match.group(0).lower()
-
         option_key = ndb.Key(model.Option, response_key_id)
         option = option_key.get()
         if option is None:
-            response.message('Response code not found. Please verify your 4-character response code.')
+            response.message('Response code not found. Please verify your 4-c\
+haracter response code.')
             return str(response)
-
-        poll_response_key = model.Response.create_response(option, str(from_number))
+        poll_response_key = model.Response.create_response(
+            option,
+            str(from_number)
+        )
         if poll_response_key:
-            response.message('Your response has been recorded, thanks for voting!\n\nText another poll\'s reponse code to vote in another.')
+            response.message('Your response has been recorded, thanks for vot\
+ing!\n\nText another poll\'s reponse code to vote in another.')
             return str(response)
         else:
-            response.message('There was an error recording your response. Please tray again later.')
+            response.message('There was an error recording your response. Ple\
+ase tray again later.')
             return str(response)
     else:
-        response.message('Response code invalid. Please send a 4-character response code.')
-
+        response.message('Response code invalid. Please send a 4-character re\
+sponse code.')
     return str(response)
 
 
